9affe5907b62539c9121302b369de2e48400d135
javi <jeg1966@telefonica.net>
2022-08-15 12:26:31 +0200

    Update skMedidorPH.ino
    Nuevo proceso medidas y añadir DS18b20

--- skMedidorPH.ino
+++ skMedidorPH.ino
@@ -1,14 +1,26 @@
 #include <ESP8266WiFi.h>
 //#include <TimerOne.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 #include "Adafruit_ADS1015.h"
 
 Adafruit_ADS1115 ads;  /* Use this for the 16-bit version */
 //Adafruit_ADS1015 ads;
 
+const int oneWireBus = 14;      //GPIO14-D5
+
+// Setup a oneWire instance to communicate with any OneWire devices
+OneWire oneWire(oneWireBus);
+
+// Pass our oneWire reference to Dallas Temperature sensor 
+DallasTemperature sensorDS18B20(&oneWire);
+
+
 #define AP_SSID "Wireless-N"
 #define AP_PASSWORD "z123456z"
 
-
+#define CH_TDS 3
+#define CH_PH 0
 #define DEBUG true
 const int LED =  13;
 const int RELE = 15;
@@ -18,25 +30,36 @@ int32_t avgValue;              //Store the average value of the sensor feedback
 bool toggle = false;
 
 float valorPH = 0.0;
+
+
+#define SCOUNT  30           // sum of sample point
+int32_t analogBuffer[SCOUNT];    // store the analog value in the array, read from ADC
+
+float averageVoltage = 0, tdsValue = 0, temperature = 25;
+
+
+
 WiFiServer server(80);
 
 
 //**********************************************************************************
 void setup()
 {
- 
+
   pinMode(RELE, OUTPUT);
   pinMode(LED, OUTPUT);
 
   Serial.begin(115200);
 
-  digitalWrite(RELE,true);
+  digitalWrite(RELE, true);
 
   ads.setGain(GAIN_TWOTHIRDS);
 
   ads.begin();
 
- 
+  sensorDS18B20.begin();  //DS18b20
+
+
   //Timer1.initialize(500000);//timing for 500ms
   // Timer1.attachInterrupt(TimingISR);//declare the interrupt serve routine:TimingISR
 
@@ -46,277 +69,260 @@ void setup()
   Serial.println("LISTO!!!");    //Test the serial monitor*/
 }
 
+
+
 //*****************************************************************************
 void loop()
 {
 
-  mideTDS();
-  delay(1000);
-
- /* 
-
-  WiFiClient client = server.available();
-
-  if (!client) {
-    return;
-  }
-  Serial.print("NUEVO CLIENTE\r\n");
-
-  while (!client.connected()) {
-
-    delay(1);
-  }
-
-  String req = client.readStringUntil('\r');
-  Serial.print(req);
-  Serial.print("\r\n");
-  client.flush();
-
-  
-
-
-  if (req.indexOf("ph/") != -1)
-  {
-
-    digitalWrite(RELE,false);
-
-    delay(1000);
-    
-    String str = "HTTP/1.1 200 OK\r\nContent-Type: text/json\r\n\r\n{'PH':";
-    String valorPH = String(midePH(), 2);
-    String cierre = "}";
-
-    Serial.println(str+valorPH+cierre);
-    client.print(str+valorPH+cierre);
-    
-    delay(1);
-    Serial.println("Client disonnected");
-
-   
-  }
-
-  else if (req.indexOf("tds/") != -1)
-  {
-    digitalWrite(RELE,true);
-
-    delay(1000);
-    
-    String str = "HTTP/1.1 200 OK\r\nContent-Type: text/json\r\n\r\n{'TDS':";
-    String valorTDS = String(mideTDS(), 2);
-    String cierre = "}";
-
-    Serial.println(str+valorTDS+cierre);
-    client.print(str+valorTDS+cierre);
-    
-    delay(1);
-    Serial.println("Client disonnected");
-   
-  }
-
-  else
-  {
-
-    Serial.println("invalid request");
-    client.stop();
-    return;
-  }
-
-*/
-
-
- /*int16_t adc0, adc1, adc2, adc3;
-
-  adc0 = ads.readADC_SingleEnded(0);
-
- Serial.print("AIN0: "); Serial.println(midePH());
-
-  delay(1000);*/
-
- 
-
-}
-//************************************************************************************************
-void TimingISR()
-{
-  static byte cntTemp = 0;
-
-  toggle = !toggle;
-  digitalWrite(LED, toggle);
-
-  cntTemp = 0;
-
-}
-
-//*******************************************************************************************
-float midePH() {
-
-    float Voltage =0;
-    int16_t sensorValue = 0;
-    const float multiplier = 0.1875F;  //ADS1115 ,GAIN_TWOTHIRDS  6,144V  0,1875mV
-
-    sensorValue=ads.readADC_SingleEnded(0);
-    Serial.print("Valor sensor A1: ");
-    Serial.println(sensorValue);
-    Voltage= sensorValue * multiplier;
-    //Voltage=Voltage/1000;
-
-    Serial.print("Voltaje: ");
-    Serial.println(Voltage);
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-
-  int16_t buf[20];                //buffer for read analog
-  for (int i = 0; i < 19; i++) //Get 10 sample value from the sensor for smooth the value
-  {
-    buf[i] = ads.readADC_SingleEnded(0);
-    delay(10);
-  }
-
-  for (int i = 0; i < 9; i++) //sort the analog from small to large
-  {
-    for (int j = i + 1; j < 10; j++)
-    {
-      if (buf[i] > buf[j])
-      {
-        int16_t temp = buf[i];
-        buf[i] = buf[j];
-        buf[j] = temp;
-      }
-    }
-  }
-
-  //for (int i = 0; i < 10; i++)
-  //  Serial.println(buf[i]);
-
-  avgValue = 0L;
-  for (int i = 0; i < 19; i++)               //take the average value of 6 center sample
-    avgValue += buf[i];
-    
-   Serial.print("Media: ");
-   Serial.println(avgValue/20);
-   
-  // float phValue = (float)avgValue * 5.0 / 1024 / 6; //convert the analog into millivolt
-  float phValue = (float)avgValue / 6.0 * 0.1875;
-
-  //phValue = 3.5 * phValue/1000 + Offset;                //convert the millivolt into pH value
-
-  phValue = -5.70 * phValue / 1000 + 21.34;
-  Serial.print("    pH:");
-  Serial.print(phValue, 1);
-  Serial.println(" ");
-
-  return phValue;
-
-}
-
-//********************************************************************************************
-float mideTDS() {
-
-  float Voltage =0;
-  float tdsValue =0;
-  int16_t sensorValue = 0;
-  const float multiplier = 0.1875F;  //ADS1115 ,GAIN_TWOTHIRDS  6,144V  0,1875mV
-  float temperature  = 25;
-
- /* int16_t buf[20];                //buffer for read analog
-  for (int i = 0; i < 10; i++) //Get 10 sample value from the sensor for smooth the value
-  {
-    buf[i] = ads.readADC_SingleEnded(1) ;
-    delay(10);
-  }
-
-  for (int i = 0; i < 9; i++) //sort the analog from small to large
-  {
-    for (int j = i + 1; j < 10; j++)
-    {
-      if (buf[i] > buf[j])
-      {
-        int16_t temp = buf[i];
-        buf[i] = buf[j];
-        buf[j] = temp;
-      }
-    }
-  }
-
-  //for (int i = 0; i < 10; i++)
-  //  Serial.println(buf[i]);
-
-  avgValue = 0L;
-  for (int i = 2; i < 8; i++)               //take the average value of 6 center sample
-    avgValue += buf[i];
-  */
-
-    delay(1000);
-    
-    sensorValue=ads.readADC_SingleEnded(3);
-    Serial.print("Valor sensor A3: ");
-    Serial.println(sensorValue);
-    
-    Voltage= sensorValue * multiplier;
-    Voltage=Voltage/1000;
-
-    Serial.print("Voltaje: ");
-    Serial.println(Voltage);
-    
      
-    float compensationCoefficient=1.0+0.02*(temperature-25.0); 
-    float compensationVoltage=Voltage/compensationCoefficient;
-    tdsValue=(133.42*compensationVoltage*compensationVoltage*compensationVoltage - 255.86*compensationVoltage*compensationVoltage + 857.39*compensationVoltage)*0.5;
-   // Serial.println(tdsValue);
+
+
+      WiFiClient client = server.available();
+
+      if (!client) {
+       return;
+      }
+      Serial.print("NUEVO CLIENTE\r\n");
+
+      while (!client.connected()) {
+
+       delay(1);
+      }
+
+      String req = client.readStringUntil('\r');
+      Serial.print(req);
+      Serial.print("\r\n");
+      client.flush();
+
+
+
+
+      if (req.indexOf("ph/") != -1)
+      {
+
+       digitalWrite(RELE,false);
+
+       delay(1000);
+
+       String str = "HTTP/1.1 200 OK\r\nContent-Type: text/json\r\n\r\n{'PH':";
+       String valorPH = String(midePH(), 2);
+       String cierre = "}";
+
+       Serial.println(str+valorPH+cierre);
+       client.print(str+valorPH+cierre);
+
+       delay(1);
+       Serial.println("Client disonnected");
+
+
+      }
+
+      else if (req.indexOf("tds/") != -1)
+      {
+       digitalWrite(RELE,true);
+
+       delay(1000);
+
+       String str = "HTTP/1.1 200 OK\r\nContent-Type: text/json\r\n\r\n{'TDS':";
+       String valorTDS = String(mideTDS(), 2);
+       String cierre = "}";
+
+       Serial.println(str+valorTDS+cierre);
+       client.print(str+valorTDS+cierre);
+
+       delay(1);
+       Serial.println("Client disonnected");
+
+      }
+      else if (req.indexOf("temperaturaTDS/") != -1)
+      {
+      
+
+       delay(1000);
+
+       String str = "HTTP/1.1 200 OK\r\nContent-Type: text/json\r\n\r\n{'TemperaturaTDS':";
+       String valorTemperatura = String(mideTemperatura(), 2);
+       String cierre = "}";
+
+       Serial.println(str+valorTemperatura+cierre);
+       client.print(str+valorTemperatura+cierre);
+
+       delay(10);
+       Serial.println("Client disonnected");
+
+      }
+
+      else
+      {
+
+       Serial.println("invalid request");
+       client.stop();
+       return;
+      }
+
   
-  
-  return tdsValue;
 
-}
+}//loop
 
-//******************************************************************************************
-void wifiConnect()
-{
+  //************************************************************************************************
+  void TimingISR()
+  {
+    static byte cntTemp = 0;
 
-  WiFi.mode(WIFI_STA);
+    toggle = !toggle;
+    digitalWrite(LED, toggle);
 
-  Serial.println("Connecting to AP");
-  WiFi.begin(AP_SSID, AP_PASSWORD);
+    cntTemp = 0;
 
-  WiFi.config(IPAddress(192, 168, 1, 252), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
-
-  int timeout = 0;
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(1000);
-    Serial.print("*");
-
-    if (++timeout > 100)
+  }
+  //***********************************************************************************************
+  int getMedianNum(int bArray[], int iFilterLen)
+  {
+    int bTab[iFilterLen];
+    for (byte i = 0; i < iFilterLen; i++)
+      bTab[i] = bArray[i];
+    int i, j, bTemp;
+    for (j = 0; j < iFilterLen - 1; j++)
     {
-      Serial.println("Sin Conexion WIFI");
-      while (1) {
-        digitalWrite(LED, LOW);
-        delay(100);
-        digitalWrite(LED, HIGH);
-        delay(100);
+      for (i = 0; i < iFilterLen - j - 1; i++)
+      {
+        if (bTab[i] > bTab[i + 1])
+        {
+          bTemp = bTab[i];
+          bTab[i] = bTab[i + 1];
+          bTab[i + 1] = bTemp;
+        }
       }
     }
+    if ((iFilterLen & 1) > 0)
+      bTemp = bTab[(iFilterLen - 1) / 2];
+    else
+      bTemp = (bTab[iFilterLen / 2] + bTab[iFilterLen / 2 - 1]) / 2;
+    return bTemp;
+  }
+
+
+  //*******************************************************************************************
+  float midePH() {
+
+    float Voltage = 0.0;
+    float phValue=0.0;
+      
+    Voltage=mideADS115(CH_PH);
+    Serial.print("Voltaje: ");
+    Serial.println(Voltage);
+    
+
+    //phValue = -5.70 * Voltage + 21.34;
+    phValue=7 - (2.5 - Voltage) * -3.8;
+    Serial.print("pH:");
+    Serial.print(phValue, 1);
+    Serial.println(" ");
+
+    return phValue;
 
   }
 
- IPAddress http_server_ip = WiFi.localIP();
+  //********************************************************************************************
+  float mideTDS() {
 
-  server.begin();
+    float temperature  = 0.0;
+    float Voltage = 0.0;
+  
 
-  Serial.println("Nuestra server IP:");
-  Serial.print(http_server_ip);
-  Serial.print("\r\n");
+    Voltage=mideADS115(CH_TDS);
+    Serial.print("Voltaje: ");
+    Serial.println(Voltage);
+
+    temperature=mideTemperatura();
 
 
-}
+    float compensationCoefficient = 1.0 + 0.02 * (temperature - 25.0);
+    float compensationVoltage = Voltage / compensationCoefficient;
+    tdsValue = (133.42 * compensationVoltage * compensationVoltage * compensationVoltage - 255.86 * compensationVoltage * compensationVoltage + 857.39 * compensationVoltage) * 0.5;
+    Serial.println(tdsValue);
+
+    return tdsValue;
+
+  }
+  //*********************************************************************************************
+  float mideADS115(int ch){
+
+    float Voltage = 0;
+    const float multiplier = 0.1875F;  //ADS1115 ,GAIN_TWOTHIRDS  6,144V  0,1875mV
+    int buf[SCOUNT];
+    
+    for(int i=0;i<SCOUNT;i++){
+      buf[i] = ads.readADC_SingleEnded(ch);    //read the analog value and store into the buffer
+      delay(10);
+    }
+
+    
+    Voltage = (getMedianNum(buf,SCOUNT) * multiplier ) /1000;
+        
+
+    //sensorValue = ads.readADC_SingleEnded(ch);
+   // Serial.print("Valor sensor: ");
+   // Serial.println(sensorValue);
+
+   
+   
+    return Voltage;
+    
+  }
+  //*******************************************************************************************
+  float mideTemperatura(){
+
+      sensorDS18B20.requestTemperatures(); 
+      float temperatureC = sensorDS18B20.getTempCByIndex(0);
+     
+      Serial.print(temperatureC);
+      Serial.println("ºC");
+
+      return temperatureC;
+
+
+    
+  }
+  
+
+  //******************************************************************************************
+  void wifiConnect()
+  {
+
+    WiFi.mode(WIFI_STA);
+
+    Serial.println("Connecting to AP");
+    WiFi.begin(AP_SSID, AP_PASSWORD);
+
+    WiFi.config(IPAddress(192, 168, 1, 252), IPAddress(192, 168, 1, 1), IPAddress(255, 255, 255, 0));
+
+    int timeout = 0;
+    while (WiFi.status() != WL_CONNECTED) {
+      delay(1000);
+      Serial.print("*");
+
+      if (++timeout > 100)
+      {
+        Serial.println("Sin Conexion WIFI");
+        while (1) {
+          digitalWrite(LED, LOW);
+          delay(100);
+          digitalWrite(LED, HIGH);
+          delay(100);
+        }
+      }
+
+    }
+
+    IPAddress http_server_ip = WiFi.localIP();
+
+    server.begin();
+
+    Serial.println("Nuestra server IP:");
+    Serial.print(http_server_ip);
+    Serial.print("\r\n");
+
+
+  }
